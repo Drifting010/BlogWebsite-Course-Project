c5ed1098777d58da147c7915c06f58c1b6c87bf7
liuyj <yanjie.liu@hotmail.com>
2023-02-01 20:41:50 +1300

    module with two functions related to auth

---
+++ modules/user-db.js
@@ -0,0 +1,34 @@
+const users = [
+    {
+        id: 0,
+        username: "user1",
+        password: "pa55word",
+        name: "Alice"
+    },
+    {
+        id: 1,
+        username: "user2",
+        password: "pa55word",
+        name: "Bob"
+    }
+];
+
+function getUserWithCredentials(username, password) {
+    return users.find(function(user) {
+        return user.username === username && user.password === password;
+    });
+}
+
+function getUserWithAuthToken(authToken) {
+    if (!authToken) {
+        return undefined;
+    }
+    return users.find(function(user) {
+        return user.authToken === authToken;
+    });
+}
+
+module.exports = {
+    getUserWithCredentials,
+    getUserWithAuthToken
+};
