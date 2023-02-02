f95e8ea74c38a30762a7519fb82e2bb9064cba62
Blam <biancahiuyinlam@gmail.com>
2023-02-02 16:23:45 +1300

    deleted second .sql file

--- sql/project-database-init-script.sql
+++ sql/project-database-init-script.sql
@@ -1,16 +1,55 @@
-/*
- * Upon submission, this file should contain the SQL script to initialize your database.
- * It should contain all DROP TABLE and CREATE TABLE statments, and any INSERT statements
- * required.
- */
+-- @Author: Hikaru Suzuki
+-- @Created: 31/01/2023
+-- @Modified: dd/mm/yyyy
 
-drop table if exists test;
 
-create table test (
-    id integer not null primary key,
-    stuff text  
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS articles;
+DROP TABLE IF EXISTS comments;
+DROP TABLE IF EXISTS likedarticles;
+
+
+-- To create the tables (DDL)
+
+
+CREATE TABLE users (
+    username VARCHAR(20) not null primary key,
+    hashed_password VARCHAR(100),
+    salt VARCHAR(100),
+    avatar_id INTEGER,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    date_of_birth DATE,
+    description TEXT
 );
 
-insert into test (stuff) values
-    ('Things'),
-    ('More things');
+CREATE TABLE articles (
+    article_id INTEGER not null primary key, 
+    title VARCHAR(255), 
+    content TEXT, 
+    image VARCHAR(255), 
+    username VARCHAR(20),
+    FOREIGN KEY (username) REFERENCES users (username)
+);
+
+CREATE TABLE comments (
+    comment_id INTEGER not null primary key, 
+    content TEXT,
+    created_datetime TIMESTAMP,
+    upvote INTEGER,
+    downvote INTEGER,
+    parent_comment_id INTEGER,
+    username VARCHAR(30),
+    article_id INTEGER,
+    FOREIGN KEY (parent_comment_id) REFERENCES comments (comment_id),
+    FOREIGN KEY (username) REFERENCES users (username),
+    FOREIGN KEY (article_id) REFERENCES articles (article_id)
+);
+
+CREATE TABLE likedarticles (
+    username VARCHAR(30),
+    article_id INTEGER,
+    PRIMARY KEY (username, article_id),
+    FOREIGN KEY (username) REFERENCES users (username),
+    FOREIGN KEY (article_id) REFERENCES articles (Article_Id)
+);
