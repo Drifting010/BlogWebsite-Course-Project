7120dabbe5051b144cbef3049a7d631094df2ed9
Drifting010 <111164112+Drifting010@users.noreply.github.com>
2023-02-02 14:42:04 +1300

    Update blog.sql

--- sql/blog.sql
+++ sql/blog.sql
@@ -3,75 +3,53 @@
 -- @Modified: dd/mm/yyyy
 
 
--- To drop tables if exists
-
--- TEST
-drop table if exists test;
-
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS articles;
 DROP TABLE IF EXISTS comments;
-DROP TABLE IF EXISTS liked-articles;
+DROP TABLE IF EXISTS likedarticles;
 
 
 -- To create the tables (DDL)
 
--- TEST
-create table test (
-    id integer not null primary key,
-    stuff text  
-);
 
 CREATE TABLE users (
-    Username VARCHAR(255) not null primary key,
-    Password NVARCHAR(100), 
-    Hashed_Password NVARCHAR(100),
-    Salted_password NVARCHAR(100),
-    Avatar_number INTEGER,
-    First_Name VARCHAR(255),
-    Last_Name VARCHAR(255),
-    Date_Of_Birth DATE,
-    Description TEXT
+    username VARCHAR(20) not null primary key,
+    hashed_password VARCHAR(100),
+    salt VARCHAR(100),
+    avatar_id INTEGER,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    date_of_birth DATE,
+    description TEXT
 );
 
 CREATE TABLE articles (
-    Article_Id INTEGER not null primary key, 
-    Title VARCHAR(255), 
-    Content TEXT, 
-    Image VARCHAR(255), 
-    FOREIGN KEY (Username) REFERENCES users (Username),
+    article_id INTEGER not null primary key, 
+    title VARCHAR(255), 
+    content TEXT, 
+    image VARCHAR(255), 
+    username VARCHAR(20),
+    FOREIGN KEY (username) REFERENCES users (username)
 );
 
 CREATE TABLE comments (
-    Comment_Id INTEGER not null primary key, 
-    Content TEXT,
-    Created_DateTime TIMESTAMP,
-    Upvote INTEGER,
-    Downvote INTEGER,
-    FOREIGN KEY (Parent_Comment_Id) REFERENCES comments (Comment_Id),
-    FOREIGN KEY (Username) REFERENCES users (Username),
-    FOREIGN KEY (Article_Id) REFERENCES articles (Article_Id)
+    comment_id INTEGER not null primary key, 
+    content TEXT,
+    created_datetime TIMESTAMP,
+    upvote INTEGER,
+    downvote INTEGER,
+    parent_comment_id INTEGER,
+    username VARCHAR(30),
+    article_id INTEGER,
+    FOREIGN KEY (parent_comment_id) REFERENCES comments (comment_id),
+    FOREIGN KEY (username) REFERENCES users (username),
+    FOREIGN KEY (article_id) REFERENCES articles (article_id)
 );
 
-CREATE TABLE liked-articles (
-    FOREIGN KEY (Username) REFERENCES users (Username),
-    FOREIGN KEY (Article_Id) REFERENCES articles (Article_Id)
+CREATE TABLE likedarticles (
+    username VARCHAR(30),
+    article_id INTEGER,
+    PRIMARY KEY (username, article_id),
+    FOREIGN KEY (username) REFERENCES users (username),
+    FOREIGN KEY (article_id) REFERENCES articles (Article_Id)
 );
-
-
--- Queries
-
--- TEST
-insert into test (stuff) values
-    ('Things'),
-    ('More things');
-
-INSERT INTO users (Username, Password, Avatar_number, First_Name, Last_Name, Date_Of_Birth, Description) VALUES
-    ('user123', 'password123', 1, 'Myfirstname', 'Mylastname', '01-01-2000', "User's name is Myfirstname Mylastname. The user name is user123.");
-
-INSERT INTO articles (Article_Id, Title, Text, Image) VALUES
-    (1, 'Hello!', 'Hello evryone, this is my first blog!', 'image.png');
-
-INSERT INTO comments (Comment_Id, Text, Created_DateTime, Upvote, Downvote) VALUES
-    (1, 'Brilliant!', '2023-01-01 00:00:00:00', 1, 1);
-
