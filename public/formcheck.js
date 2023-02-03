30ba31b213e2c8c89c2ab1f11ad2e8c18493be2b
liuyj <yanjie.liu@hotmail.com>
2023-02-03 15:26:31 +1300

    username check front end part completed

--- public/formcheck.js
+++ public/formcheck.js
@@ -1,15 +1,32 @@
 window.addEventListener("load", function (event) {
 
     // hint display
-    const username = document.querySelector("#usernamecheck");
-
+    const usernameChecckDiv = this.document.querySelector("#usernamecheck");
     const pwdCheckDiv = document.querySelector("#pwdcheck");
 
-    // 
+    const username = document.querySelector("#txtUsername");
     const pwdOne = this.document.querySelector("#txtPassword");
     const pwdTwo = this.document.querySelector("#txtPasswordConfirm");
 
-    pwdTwo.addEventListener("keydown", pwdCheck);
+    username.addEventListener("blur", usernameCheck);
+    pwdTwo.addEventListener("blur", pwdCheck);
+
+
+    async function usernameCheck() {
+        const reponse = await fetch("./usenamecheck");
+        const userJson = await reponse.json();
+
+        userJson.array.forEach(element => {
+            if(element.username === username.value) {
+                usernameChecckDiv.innerText = "Username Check Passes!";
+                return true;
+            } else {
+                usernameChecckDiv.innerText = "Username Already Taken!";
+            }
+        });
+
+        return false;
+    }
 
     function pwdCheck() {
         if (pwdOne.value != '' && pwdOne.value == pwdTwo.value) {
