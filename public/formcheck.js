d93fbe688eac5a7c7fb28c7fb13d8db82431d486
liuyj <yanjie.liu@hotmail.com>
2023-02-03 14:53:00 +1300

    password match check feature COMPLETED!!

--- public/formcheck.js
+++ public/formcheck.js
@@ -2,26 +2,23 @@ window.addEventListener("load", function (event) {
 
     // hint display
     const username = document.querySelector("#usernamecheck");
-    const pwdCheckDisplay = document.querySelector("#pwdcheck");
+
+    const pwdCheckDiv = document.querySelector("#pwdcheck");
 
     // 
     const pwdOne = this.document.querySelector("#txtPassword");
     const pwdTwo = this.document.querySelector("#txtPasswordConfirm");
 
-    pwdCheckDisplay.addEventListener("blur", pwdCheck);
+    pwdTwo.addEventListener("keydown", pwdCheck);
 
     function pwdCheck() {
-        if (pwdOne != '' && pwdOne == pwdTwo) {
-            pwdCheckDisplay.innerText = "Password Matches!";
+        if (pwdOne.value != '' && pwdOne.value == pwdTwo.value) {
+            pwdCheckDiv.innerText = "Password Matches!";
             return true;
         } else {
-            pwdCheckDisplay.innerText = "Password Must Match!";
+            pwdCheckDiv.innerText = "Password Must Match!";
             return false;
         }
     }
-
-
-
-
 
 });
