e8529ca8cd03709d2b4eb1d2c2690b78b4d71815
liuyj <yanjie.liu@hotmail.com>
2023-02-07 10:38:08 +1300

    delete article feature ready to test

--- public/article.js
+++ public/article.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", function (event) {
-    
+    // EDIT ARTICLE
     // Send article_id to router once the edit button is clicked
     const editButtons = document.querySelectorAll("button.editArticleButton");
     const editInputHidden = document.querySelectorAll("input.editHidden");
@@ -10,6 +10,17 @@ window.addEventListener("load", function (event) {
         editButtons[i].addEventListener("click", function () {
             article_id = editButtons[i].id;
             hiddenInput = editInputHidden[i];
+            hiddenInput.setAttribute("value", article_id);
+        });
+    }
+
+    // DELETE ARTICLE
+    const deleteButtons = document.querySelectorAll("button.deleteArticleButton");
+    const deleteInputHidden = document.querySelectorAll("input.deleteHidden");
+    for (let i = 0; i < deleteButtons.length; i++) {
+        deleteButtons[i].addEventListener("click", function () {
+            article_id = deleteButtons[i].id;
+            hiddenInput = deleteInputHidden[i];
             hiddenInput.setAttribute("value", article_id);
         });
     }
