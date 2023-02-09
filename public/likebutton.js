a7a26ceb553db901a64cabf91583200908d64ed3
harvey <hguo261@aucklanduni.ac.nz>
2023-02-09 17:56:47 +1300

    uploading image

--- public/likebutton.js
+++ public/likebutton.js
@@ -1,18 +1,55 @@
 window.addEventListener("load", function () {
+    
+    console.log(`App listening on port!`)
+
+    const likeButtons = document.querySelectorAll("#likeButton");
 
 
-    likeButton.addEventListener("click", function (event) {
+    likeButtons.forEach(function(likeButton){
+        console.log(likeButton);
 
-        const img = document.querySelector("#likeButton");
-        img.src = "/images/thumbup2.jpg";
+        likeButton.addEventListener("click", function (event) {
+            console.log("Is clicked...");
+            // TODO: make the upvote recorded in the database and then change image
+            console.log(event.target.src);
+            if (event.target.src === "http://localhost:3000/images/thumbup1.jpg"){
+                console.log("Is true...");
+                event.target.src = "/images/thumbup2.jpg"
+            } else {
+                event.target.src = "/images/thumbup1.jpg"
+            }
+
+
+            // event.target.src = "/images/thumbup2.jpg";
+            
+            
+
+        });
+
 
     });
 
-    dislikeButton.addEventListener("click", function (event) {
+    // dislikeButtons.forEach(function(dislikeButton){
+    //     console.log(dislikeButton);
 
-        const img2 = document.querySelector("#dislikeButton");
-        img2.src = "/images/thumbdown2.jpg";
+    //     dislikeButton.addEventListener("click", function (event) {
 
-    });
+    //         //const img = document.querySelector("#likeButton");
+    //         event.target.src = "/images/thumbdown2.jpg";
+
+    //     });
+
+
+    // });
+
+ 
+    
+
+    // dislikeButton.addEventListener("click", function (event) {
+
+    //     const img2 = document.querySelector("#dislikeButton");
+    //     img2.src = "/images/thumbdown2.jpg";
+
+    // });
 
 });
